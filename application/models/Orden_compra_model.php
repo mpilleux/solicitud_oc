7b0f93d76e33064cf729502c8e86e53c09dda51c
mpilleux <mpilleuxg@gmail.com>
2017-05-29 18:23:37 -0400

    update de formato fecha

--- application/models/Orden_compra_model.php
+++ application/models/Orden_compra_model.php
@@ -8,16 +8,27 @@ class Orden_compra_model extends CI_Model {
 		$this->load->database();
 	}
 
+	private function convertDate($date) {
+	   // EN-Date to GE-Date
+	   if (strstr($date, "-") || strstr($date, "/"))   {
+			   $date = preg_split("/[\/]|[-]+/", $date);
+			   $date = $date[2]."/".$date[1]."/".$date[0];
+			   return $date;
+	   }
+	  
+	   return false;
+	}
 
 	public function postOC($proveedor, $descripcion, $fecha_entrega, $monto, $centro_costo){
 		$this->db->trans_start();
 		
+		$fecha = $this->convertDate($fecha_entrega);
 				
 		$sql = '
 			INSERT INTO orden_compra ( id, proveedor, descripcion, fecha_entrega, monto, centro_costo)
 			VALUES ( null, ?, ?, ?, ?, ? );
 			';
-		$this->db->query( $sql, array($proveedor, $descripcion, $fecha_entrega, $monto, $centro_costo) );
+		$this->db->query( $sql, array($proveedor, $descripcion, $fecha, $monto, $centro_costo) );
 		
 		$oc_id = $this->db->insert_id();
 		
